4b776ac40de914cb363bd4a3455f1c7f6f80f2fd
sandmark <sandmark@orangegarage.(none)>
2010-12-16 11:04:40 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KifuCommetter::Application.config.secret_token = '8a1e35de42afafb8c6b069b8be915cfa068c53002ee2b3022073cb67bb59a96313b9fdf3243c39ea3cce165c7b2bc8fd59badcb9052bd30405ab3dec581b85c4'
