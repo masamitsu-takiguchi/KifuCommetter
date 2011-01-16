2ab5df5648d3c7a25e9645cb9f2526057eb56479
sandmark <sandmark.m@gmail.com>
2011-01-16 15:00:48 +0900

    フォームを追加できないバグを修正

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -22,9 +22,9 @@ class FormsController < ApplicationController
     @form = Form.find_by_name params[:form][:name]
     @form = Form.new params[:form] if not @form
     @kifu_document = KifuDocument.find params[:form][:kifu_document_id]
-    if @form.kifu_documents.find(@kifu_document.id)
-      redirect_to kifu_document_url(@kifu_document), :notice => "既に追加されています"
-    else
+    begin
+      @form.kifu_documents.find(@kifu_document.id)
+    rescue ActiveRecord::RecordNotFound => e
       @form.kifu_documents << @kifu_document
       if @form.save
         @comments = @kifu_document.comments
@@ -35,6 +35,8 @@ class FormsController < ApplicationController
         @comment = Comment.new
         render :template => 'kifu_documents/show', :locals => {:kifu_document => @kifu_document}
       end
+    else
+      redirect_to kifu_document_url(@kifu_document), :notice => "既に追加されています"
     end
   end
   
